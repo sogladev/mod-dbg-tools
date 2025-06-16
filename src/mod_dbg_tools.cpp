8bc3aca01385608240a79f231fb0dc5608b387d8
Jelle Meeus <sogladev@gmail.com>
2025-06-16 20:22:06 +0200

    feat: scourge invasion

--- src/mod_dbg_tools.cpp
+++ src/mod_dbg_tools.cpp
@@ -72,10 +72,15 @@ public:
             {"yoggbraindmg", HandleStartYoggP3Command, SEC_ADMINISTRATOR, Console::No},
         };
 
+        static ChatCommandTable DbgToolsSiCommandTable = {
+            {"despawnnecropolis", HandleStartDespawnNecropolisCommand, SEC_ADMINISTRATOR, Console::No},
+        };
+
         static ChatCommandTable DbgToolsCommandTable = {
             {"reset", DbgToolsResetCommandTable},
             {"swp",   DbgToolsSwpCommandTable  },
-            {"uld",   DbgToolsUldCommandTable  }
+            {"uld",   DbgToolsUldCommandTable  },
+            {"si",    DbgToolsSiCommandTable   }
         };
 
         static ChatCommandTable DbgToolsBaseTable = {
@@ -85,6 +90,7 @@ public:
         return DbgToolsBaseTable;
     }
 
+    // .dbg reset dailies
     static bool HandleResetDailiesCommand(ChatHandler* handler)
     {
         Player* player = handler->GetSession()->GetPlayer();
@@ -97,6 +103,7 @@ public:
     }
 
     // Vapor
+    // .dbg swp vapor
     static bool HandleCastVaporCommand(ChatHandler* handler)
     {
         Player* player = handler->GetSession()->GetPlayer();
@@ -111,6 +118,7 @@ public:
     }
 
     // Action to start Intro Event
+    // .dbg swp mad
     static bool HandleResetMadrigosaCommand(ChatHandler* handler)
     {
         Player* player = handler->GetSession()->GetPlayer();
@@ -129,6 +137,7 @@ public:
     }
 
     // Action to spawn Felmyst
+    // .dbg swp felmyst
     static bool HandleSpawnFelmystCommand(ChatHandler* handler)
     {
         Player* player = handler->GetSession()->GetPlayer();
@@ -147,6 +156,7 @@ public:
     }
 
     // Action to start Yogg-Saron P3 (skip entering portal to kill tentacles/brain)
+    // .dbg uld yoggbraindmg
     static bool HandleStartYoggP3Command(ChatHandler* handler)
     {
         Player* player = handler->GetSession()->GetPlayer();
@@ -163,6 +173,78 @@ public:
             handler->PSendSysMessage("[DBG]::Player::HandleStartYoggP3Command Not found!");
         return true;
     }
+
+    enum SINecropolisObjects
+    {
+        GO_NECROPOLIS_TINY = 181154,   // Necropolis (scale 1.0).
+        GO_NECROPOLIS_SMALL = 181373,  // Necropolis (scale 1.5).
+        GO_NECROPOLIS_MEDIUM = 181374, // Necropolis (scale 2.0).
+        GO_NECROPOLIS_BIG = 181215,    // Necropolis (scale 2.5).
+        GO_NECROPOLIS_HUGE = 181223,   // Necropolis (scale 3.5).
+    };
+
+    // .dbg si despawnnecropolis <option>
+    static bool HandleStartDespawnNecropolisCommand(ChatHandler* handler, uint32 option)
+    {
+        Player* player = handler->GetSession()->GetPlayer();
+
+        if (!player)
+            return false;
+
+        std::list<GameObject*> necropolisList;
+        player->GetGameObjectListWithEntryInGrid(necropolisList,
+            {GO_NECROPOLIS_TINY, GO_NECROPOLIS_SMALL, GO_NECROPOLIS_MEDIUM, GO_NECROPOLIS_BIG, GO_NECROPOLIS_HUGE},
+            30.0f // ATTACK_DISTANCE
+        );
+        if (necropolisList.empty())
+        {
+            handler->PSendSysMessage("[DBG]::Player::HandleStartDespawnNecropolisCommand Not found!");
+            return false;
+        }
+
+        for (GameObject* const& necropolis : necropolisList)
+        {
+            if (option == 1)
+            {
+                necropolis->DespawnOrUnsummon(5s);
+                handler->PSendSysMessage("[DBG]::Player::HandleStartDespawnNecropolisCommand DespawnOrUnsummon 5s delay");
+            }
+            else if (option == 2)
+            {
+                necropolis->UseDoorOrButton();
+                handler->PSendSysMessage("[DBG]::Player::HandleStartDespawnNecropolisCommand UseDoorOrButton");
+            }
+            else if (option == 3)
+            {
+                necropolis->ResetDoorOrButton();
+                handler->PSendSysMessage("[DBG]::Player::HandleStartDespawnNecropolisCommand ResetDoorOrButton");
+            }
+            else if (option == 4)
+            {
+                necropolis->DespawnOrUnsummon();
+                handler->PSendSysMessage("[DBG]::Player::HandleStartDespawnNecropolisCommand DespawnOrUnsummon no delay");
+            }
+            else if (option == 5)
+            {
+                necropolis->DespawnOrUnsummon();
+                necropolis->SetRespawnDelay(-1); // Disable respawn.
+                handler->PSendSysMessage("[DBG]::Player::HandleStartDespawnNecropolisCommand DespawnOrUnsummon option 5");
+            }
+            else if (option == 6)
+            {
+                necropolis->DespawnOrUnsummon(10s);
+                necropolis->SetRespawnDelay(-1); // Disable respawn.
+                handler->PSendSysMessage("[DBG]::Player::HandleStartDespawnNecropolisCommand DespawnOrUnsummon option 5");
+            }
+            else
+            {
+                handler->PSendSysMessage("[DBG]::Player::HandleStartDespawnNecropolisCommand Invalid option!");
+                return false;
+            }
+        }
+
+        return true;
+    }
 };
 
 void AddSC_dbg_tools_script()
@@ -171,3 +253,5 @@ void AddSC_dbg_tools_script()
     new DbgToolsPlayer();
     new DbgToolsCommand();
 }
+
+void DespawnNecropolis() { }
